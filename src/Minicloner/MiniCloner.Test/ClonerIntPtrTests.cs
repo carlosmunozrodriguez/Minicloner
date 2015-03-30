27649fdb03af953ac607296c768edadc18f76370
munoz.carlos <carlosmunozrodriguez@outlook.com>
2015-03-30 13:57:54 -0500

    Initial code Clones primitive and simple value types only

---
+++ src/Minicloner/MiniCloner.Test/ClonerIntPtrTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Minicloner;
+using Xunit;
+
+namespace MiniCloner.Test
+{
+    public class ClonerIntPtrTests
+    {
+        [Fact]
+        public void CloneIntPtr()
+        {
+            var someIntPtr = (IntPtr)1;
+            var cloned = new Cloner().Clone(someIntPtr);
+
+            Assert.IsType<IntPtr>(cloned);
+            Assert.Equal(someIntPtr, cloned);
+        }
+
+        [Fact]
+        public void CloneUIntPtr()
+        {
+            var someUIntPtr = (UIntPtr)1;
+            var cloned = new Cloner().Clone(someUIntPtr);
+
+            Assert.IsType<UIntPtr>(cloned);
+            Assert.Equal(someUIntPtr, cloned);
+        }
+    }
+}
---
+++ src/Minicloner/Minicloner/Cloner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Minicloner
+{
+    public class Cloner : ICloner
+    {
+        public T Clone<T>(T source)
+        {
+            if (source == null || source.GetType().IsValueType)
+            {
+                return source;
+            }
+
+            return (T)CloneRefenceType(source);
+        }
+
+        private object CloneRefenceType(object source)
+        {
+            if (source == null)
+            {
+                return null;
+            }
+
+            // We really mean cloning here so if source is string use native String.Copy so string is not interned.
+            // TODO: Add an optional options object to Cloner's constructor to allow for optional string interning.
+            var s = source as string;
+            if (s != null)
+            {
+                return String.Copy(s);
+            }
+
+            var cloned = FormatterServices.GetUninitializedObject(source.GetType());
+
+            // TODO: Copy fields, and property values to cloned object
+            // TODO: Deep clone recursively
+            // TODO: Allow circular references
+
+            return cloned;
+        }
+    }
+}
