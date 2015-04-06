40c0f26052e673f3b54fbf4b64633e15c2b4a1de
Carlos Muñoz Rodriguez <carlosmunozrodriguez@outlook.com>
2015-04-05 22:38:44 -0500

    #6 Added support for n-dimensional arrays

--- src/Minicloner/MiniCloner.Test/ClonerArrayTests.cs
+++ src/Minicloner/MiniCloner.Test/ClonerArrayTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Minicloner;
 using Xunit;
 
@@ -26,10 +27,67 @@ namespace MiniCloner.Test
 
             Assert.IsType<Int32[]>(cloned);
             Assert.NotSame(array, cloned);
+            Assert.Equal(array.Rank, cloned.Rank);
             Assert.Equal(array.Length, cloned.Length);
-            for (int i = 0; i < array.Length; i++)
+
+            for (var i = 0; i < array.Length; i++)
             {
                 Assert.Equal(array[i], cloned[i]);
+            }
+        }
+
+        [Fact]
+        public void CloneTwoDimensionalArray()
+        {
+            Int32[,] array2D = {
+                                { 1, 2 },
+                                { 3, 4 },
+                                { 5, 6 }
+                               };
+            var cloned = new Cloner().Clone(array2D);
+
+            Assert.IsType<Int32[,]>(cloned);
+            Assert.NotSame(array2D, cloned);
+            Assert.Equal(array2D.Rank, cloned.Rank);
+            Assert.Equal(array2D.GetLength(0), cloned.GetLength(0));
+            Assert.Equal(array2D.GetLength(1), cloned.GetLength(1));
+
+            for (var i = 0; i < array2D.GetLength(0); i++)
+            {
+                for (var j = 0; j < array2D.GetLength(1); j++)
+                {
+                    Assert.Equal(array2D[i, j], cloned[i, j]);
+                }
+            }
+        }
+
+        [Fact]
+        public void CloneThreeDimensionalArray()
+        {
+            Int32[, ,] array3D = {
+                                { { 1, 2 }, { 3, 4 }, { 5, 6 } },
+                                { { 7, 8 }, { 9, 10 }, { 11, 12 } },
+                                { { 13, 14 }, { 15, 16 }, { 17, 18 } },
+                                { { 19, 20 }, { 21, 22 }, { 23, 24 } }
+                               };
+            var cloned = new Cloner().Clone(array3D);
+
+            Assert.IsType<Int32[, ,]>(cloned);
+            Assert.NotSame(array3D, cloned);
+            Assert.Equal(array3D.Rank, cloned.Rank);
+            Assert.Equal(array3D.GetLength(0), cloned.GetLength(0));
+            Assert.Equal(array3D.GetLength(1), cloned.GetLength(1));
+            Assert.Equal(array3D.GetLength(2), cloned.GetLength(2));
+
+            for (var i = 0; i < array3D.GetLength(0); i++)
+            {
+                for (var j = 0; j < array3D.GetLength(1); j++)
+                {
+                    for (var k = 0; k < array3D.GetLength(2); k++)
+                    {
+                        Assert.Equal(array3D[i, j, k], cloned[i, j, k]);
+                    }
+                }
             }
         }
     }
