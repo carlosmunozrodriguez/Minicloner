9fd546536b813b787e51cac38613cc181227ea5d
Carlos Muñoz Rodriguez <carlosmunozrodriguez@outlook.com>
2015-04-20 02:48:34 -0500

    Moved to correct folders

---
+++ src/Minicloner/Minicloner.Tests.OtherAssembly/Fakes/Class_With_ProtectedInternalField_From_OtherAssembly.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Minicloner.Tests.OtherAssembly.Fakes
+{
+    public class Class_With_ProtectedInternalField_From_OtherAssembly
+    {
+        protected internal Int32 ProtectedInternalField;
+
+        public Class_With_ProtectedInternalField_From_OtherAssembly(Int32 parameter)
+        {
+            ProtectedInternalField = parameter;
+        }
+
+        public Int32 Get_ProtectedInternalField()
+        {
+            return ProtectedInternalField;
+        }
+    }
+}
