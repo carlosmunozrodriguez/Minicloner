e089d28cb0e739645adebac83e27eed849a4fb68
Carlos Muñoz Rodriguez <carlosmunozrodriguez@outlook.com>
2018-05-11 02:39:56 -0500

    Using the whole SDK not only runtime for 1.1.9

--- build.ps1
+++ build.ps1
@@ -2,14 +2,14 @@ Import-Module ./invoke-task.psm1
 
 Invoke-Task AutomatedBuild {
     Invoke-Task Get-DotNet-Sdk {
-        $dotnetRuntimeVersion1 = "1.1.8"
+        $dotnetSdkVersion1 = "1.1.9"
         $dotnetSdkVersion2 = "2.1.200"
 
         if ($IsLinux -or $IsOsX) {
-            bash ./dotnet-install.sh -Channel Current -Version $dotnetRuntimeVersion1 -Runtime dotnet
+            bash ./dotnet-install.sh -Channel Current -Version $dotnetSdkVersion1
             bash ./dotnet-install.sh -Channel Current -Version $dotnetSdkVersion2
         } else {
-            ./dotnet-install.ps1 -Channel Current -Version $dotnetRuntimeVersion1  -Runtime dotnet
+            ./dotnet-install.ps1 -Channel Current -Version $dotnetSdkVersion1
             ./dotnet-install.ps1 -Channel Current -Version $dotnetSdkVersion2
         }
     }
