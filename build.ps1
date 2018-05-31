7dcd526a0595bce781c319138276cf277fd1301c
Carlos Muñoz Rodriguez <carlosmunozrodriguez@outlook.com>
2018-05-31 15:30:35 -0500

    Updated dotnet-install

--- build.ps1
+++ build.ps1
@@ -2,14 +2,14 @@ Import-Module ./invoke-task.psm1
 
 Invoke-Task AutomatedBuild {
     Invoke-Task Get-DotNet-Sdk {
-        $dotnetSdkVersion1 = "1.1.9"
+        $dotnetSdkVersion1 = "1.0.11"
         $dotnetSdkVersion2 = "2.1.200"
 
         if ($IsLinux -or $IsOsX) {
-            bash ./dotnet-install.sh -Channel Current -Version $dotnetSdkVersion1
-            bash ./dotnet-install.sh -Channel Current -Version $dotnetSdkVersion2
+            bash ./dotnet-install.sh --channel Current --version $dotnetSdkVersion1 --runtime dotnet
+            bash ./dotnet-install.sh --channel Current --version $dotnetSdkVersion2
         } else {
-            ./dotnet-install.ps1 -Channel Current -Version $dotnetSdkVersion1
+            ./dotnet-install.ps1 -Channel Current -Version $dotnetSdkVersion1 -Runtime dotnet
             ./dotnet-install.ps1 -Channel Current -Version $dotnetSdkVersion2
         }
     }
