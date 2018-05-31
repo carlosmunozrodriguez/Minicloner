c12955fe8d47ed87b259c309f5c8276f1118500b
Carlos Muñoz Rodriguez <carlosmunozrodriguez@outlook.com>
2018-05-31 18:40:17 -0500

    Try running dotnet-install.sh first

--- build.ps1
+++ build.ps1
@@ -2,11 +2,11 @@ Import-Module ./invoke-task.psm1
 
 Invoke-Task AutomatedBuild {
     Invoke-Task Get-DotNet-Sdk {
-        $dotnetSdkVersion1 = "1.0.11"
+        $dotnetSdkVersion1 = "1.1.9"
         $dotnetSdkVersion2 = "2.1.200"
 
         if ($IsLinux -or $IsOsX) {
-            bash ./dotnet-install.sh --channel Current --version $dotnetSdkVersion1 --runtime dotnet
+            bash ./dotnet-install.sh --channel Current --version $dotnetSdkVersion1
             bash ./dotnet-install.sh --channel Current --version $dotnetSdkVersion2
         } else {
             ./dotnet-install.ps1 -Channel Current -Version $dotnetSdkVersion1 -Runtime dotnet
