ea8a43b5f5acf26f9c9dc02a18f03f7540724555
Carlos Muñoz Rodriguez <carlosmunozrodriguez@outlook.com>
2021-09-20 03:10:23 -0500

    #139 Added tests for anonymous types

---
+++ Minicloner.Tests/CloneAnonymousTypesTests.cs
@@ -0,0 +1,29 @@
+﻿using Minicloner.Tests.Fakes.Fields;
+using Xunit;
+
+namespace Minicloner.Tests
+{
+    public class CloneAnonymousTypesTests
+    {
+        [Fact]
+        public void Clone_AnonymousClass_With_PrimitiveValueTypeProperty()
+        {
+            var anonymousTypedObject = new { Int32 = 1 };
+            var cloned = new Cloner().Clone(anonymousTypedObject);
+
+            Assert.NotSame(anonymousTypedObject, cloned);
+            Assert.Equal(anonymousTypedObject.Int32, cloned.Int32);
+        }
+
+        [Fact]
+        public void Clone_AnonymousClass_With_ReferenceTypeProperty()
+        {
+            var anonymousTypedObject = new { Object = new Class_With_PublicField { PublicField = 1 } };
+            var cloned = new Cloner().Clone(anonymousTypedObject);
+
+            Assert.NotSame(anonymousTypedObject, cloned);
+            Assert.NotSame(anonymousTypedObject.Object, cloned.Object);
+            Assert.Equal(anonymousTypedObject.Object.PublicField, cloned.Object.PublicField);
+        }
+    }
+}
